9044c84268acb202b5378a88ee66160070972d9c
Taras Noga <taraskin2012@gmail.com>
2022-05-20 15:06:26 +0300

    add new function to modal window

--- js/script.js
+++ js/script.js
@@ -105,13 +105,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalCloseBtn = document.querySelector("[data-close]");
   const modalWindow = document.querySelector(".modal");
 
+  function openModal() {
+    modalWindow.classList.toggle("show");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      // modalWindow.classList.add("show");
-      // modalWindow.classList.remove("hide");
-      modalWindow.classList.toggle("show");
-      document.body.style.overflow = "hidden";
-    });
+    btn.addEventListener("click", openModal);
   });
 
   function closeModal() {
@@ -132,4 +133,16 @@ document.addEventListener("DOMContentLoaded", function () {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
 });
