33c6207e7ad1b097e640fa47bcc300a0e4221e17
Taras Noga <taraskin2012@gmail.com>
2022-05-26 23:02:25 +0300

    add fetch

--- js/script.js
+++ js/script.js
@@ -256,10 +256,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
       form.insertAdjacentElement("afterend", statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
-
-      request.setRequestHeader("Content-type", "application/json");
       //Якщо використовується XMLHttpRequest + FormData, тоді встановлювати заголовки не потрібно
 
       const formData = new FormData(form);
@@ -269,20 +265,25 @@ document.addEventListener("DOMContentLoaded", function () {
         object[key] = value;
       });
 
-      const json = JSON.stringify(object);
-
-      request.send(json);
-
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
           statusMessage.remove();
-          form.reset();
-        } else {
+        })
+        .catch(() => {
           showThanksModal(message.fail);
-        }
-      });
+        })
+        .finally(() => {
+          form.reset();
+        });
     });
   }
 
