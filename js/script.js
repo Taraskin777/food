7e99a38b6ed2d49f08128ad160e23cf2f1583846
Taras Noga <taraskin2012@gmail.com>
2022-06-01 13:29:56 +0300

    add another slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // const { default: axios } = require("axios");
 
 document.addEventListener("DOMContentLoaded", function () {
-  
   // tabs
 
   const tabs = document.querySelectorAll(".tabheader__item");
@@ -319,55 +318,121 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".offer__slide"),
     prev = document.querySelector(".offer__slider-prev"),
     next = document.querySelector(".offer__slider-next"),
-    image = document.querySelector(".offer__slider-wrapper"),
+    // image = document.querySelector(".offer__slider-wrapper"),
     total = document.querySelector("#total"),
-    curr = document.querySelector("#current");
+    curr = document.querySelector("#current"),
+    slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+    slidesField = document.querySelector(".offer__slider-inner"),
+    width = window.getComputedStyle(slidesWrapper).width;
 
   let slideIndex = 1;
-
-  showSlides(slideIndex);
+  let offset = 0;
 
   if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
+    curr.textContent = `0${slideIndex}`;
   } else {
     total.textContent = slides.length;
+    curr.textContent = slideIndex;
   }
 
-  function showSlides(n) {
-    if (n > slides.length) {
+  slidesField.style.width = 100 * slides.length + "%";
+  slidesField.style.display = "flex";
+  slidesField.style.transition = "0.5s all";
+  slidesWrapper.style.overflow = "hidden";
+
+  slides.forEach((slide) => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener("click", () => {
+    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
       slideIndex = 1;
+    } else {
+      slideIndex++;
     }
-
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach((item) => {
-      item.style.display = "none";
-    });
-
-    slides[slideIndex - 1].style.display = "block";
 
     if (slides.length < 10) {
       curr.textContent = `0${slideIndex}`;
     } else {
       curr.textContent = slideIndex;
     }
-  }
-
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
+  });
 
   prev.addEventListener("click", () => {
-    plusSlides(-1);
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      curr.textContent = `0${slideIndex}`;
+    } else {
+      curr.textContent = slideIndex;
+    }
   });
 
-  next.addEventListener("click", () => {
-    plusSlides(1);
-  });
+  //простий слайдер
 
-  image.addEventListener("click", () => {
-    plusSlides(1);
-  });
+  // showSlides(slideIndex);
+
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`;
+  // } else {
+  //   total.textContent = slides.length;
+  // }
+
+  // function showSlides(n) {
+  //   if (n > slides.length) {
+  //     slideIndex = 1;
+  //   }
+
+  //   if (n < 1) {
+  //     slideIndex = slides.length;
+  //   }
+
+  //   slides.forEach((item) => {
+  //     item.style.display = "none";
+  //   });
+
+  //   slides[slideIndex - 1].style.display = "block";
+
+  //   if (slides.length < 10) {
+  //     curr.textContent = `0${slideIndex}`;
+  //   } else {
+  //     curr.textContent = slideIndex;
+  //   }
+  // }
+
+  // function plusSlides(n) {
+  //   showSlides((slideIndex += n));
+  // }
+
+  // prev.addEventListener("click", () => {
+  //   plusSlides(-1);
+  // });
+
+  // next.addEventListener("click", () => {
+  //   plusSlides(1);
+  // });
+
+  // image.addEventListener("click", () => {
+  //   plusSlides(1);
+  // });
 });
